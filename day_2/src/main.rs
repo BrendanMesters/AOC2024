c42fb1e33efe807471368f8090da8981419a2572
Brendan Mesters @BreViBo <brendanmesters@gmail.com>
2024-12-03 11:47:33 +0100

    finished day 2
    finished day 2 (and some gitignore stuff)
    
    Quite happy with the continue to tag solution I used for some failure
    checking code :-)

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
 use std::fs;
 use std::process;
-use std::str::FromStr;
 
 fn main() {
     let (part, input_file): (usize, String) = parse_input();
@@ -21,23 +20,94 @@ fn main() {
     }
 }
 
-fn part_one(_contents: String) {
+fn part_one(contents: String) {
     //! Runs **part one** of this weeks assigment
     //!
     //! Takes the input string as an argument and
     //! should print out the result.
+    println!("In part 1");
 
-    let result: usize = 0;
+    let mut result: usize = 0;
+
+    'reports: for report in contents.lines() {
+        // read in data
+        let levels: Vec<usize>  = report.split_whitespace()
+            .map(
+                |v| 
+                v.parse::<usize>().unwrap_or_else(
+                    |_e| {
+                        eprintln!("The input file was formatted incorrectly, it should only contain numbers seperated by whitespace characters"); 
+                        process::exit(1)
+                    }
+                )
+            ).collect();
+
+        // calculate safe and unsafe reports
+        let increasing: bool = levels[0] < levels[1];
+        for i in 0..(levels.len() - 1) {
+            let diff: usize = levels[i].abs_diff(levels[i + 1]);
+            if (1 > diff || 3 < diff) ||  ((levels[i] > levels[i + 1]) == increasing) {
+                continue 'reports;
+            }
+        }
+        result += 1;
+    }
+
     println!("result part 1: {result}")
 }
 
-fn part_two(_contents: String) {
+fn part_two(contents: String) {
     //! Runs **part two** of this weeks assigment
     //!
     //! Takes the input string as an argument and
     //! should print out the result.
+    println!("In part 2");
+    
 
-    let result: usize = 0;
+    let mut result: usize = 0;
+
+    'reports: for report in contents.lines() {
+        // read in data
+        let levels: Vec<usize>  = report.split_whitespace()
+            .map(
+                |v| 
+                v.parse::<usize>().unwrap_or_else(
+                    |_e| {
+                        eprintln!("The input file was formatted incorrectly, it should only contain numbers seperated by whitespace characters"); 
+                        process::exit(1)
+                    }
+                )
+            ).collect();
+
+
+        'deletion: for d in 0..=(levels.len()) {
+            let mut lv: Vec<usize> = levels.clone();
+            if d > 0 {
+                lv.remove(d - 1);
+            }
+            for v in lv.clone(){
+                print!("{v} ");
+            }
+
+            // checks this subset
+            let increasing: bool = lv[0] < lv[1];
+            for i in 0..(lv.len() - 1) {
+                let diff: usize = lv[i].abs_diff(lv[i + 1]);
+                if (1 > diff || 3 < diff) ||  ((lv[i] > lv[i + 1]) == increasing) {
+                    // failure
+                    println!("    failed at index {i}");
+                    continue 'deletion;
+                }
+            }
+            println!("    succeeded!");
+            // succes
+            result += 1;
+            continue 'reports;
+        }
+        println!("This loop was a total failure");
+
+
+    }
     println!("result part 2: {result}");
 }
 
@@ -56,7 +126,7 @@ fn parse_input() -> (usize, String) {
         process::exit(1);
     }
 
-    let part = args[1].parse::<usize>().unwrap_or_else(|e| {
+    let part = args[1].parse::<usize>().unwrap_or_else(|_e| {
         eprintln!("Error, part value should be a valid integer");
         process::exit(1);
     });
